7c07545ba597dc75cbf7b00844b82eb3383462b9
Elizaveta Goldova <lizagoldova13@gmail.com>
2022-10-01 16:10:23 +0300

    hw1

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.androidlab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import com.example.androidlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.edit.setOnClickListener{
+            if(binding.avatar.isVisible) {
+                binding.avatar.visibility = View.GONE
+            } else { binding.avatar.visibility = View.VISIBLE}
+        }
     }
 }
